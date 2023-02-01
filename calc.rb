5592eb27e914464cfbebcc07fc7ef3979ab1aafa
atsuri <atsuri.snoopy@gmail.com>
2023-02-01 10:50:07 +0900

    ver2.0

--- calc.rb
+++ calc.rb
@@ -7,7 +7,7 @@ class Calc
         rescue
             puts "計算式がありません。"
         end
-        scanner = StringScanner.new(@formula.chomp)
+        @scanner = StringScanner.new(@formula.chomp)
         # puts scanner
 
         @keywords = {
@@ -19,43 +19,37 @@ class Calc
             ')' => :right_parn
         }
 
-        @scanner = []
-        tokenize(scanner)
-        # p @scanner
-
-        p eval(expression) #結果を出力
+        p eval(parse()) #結果を出力
     end
 
-    def tokenize(scanner)
-        scan = scanner.scan(/(\d+|[\+\-\*\/()])*/)
-        # puts scan
-        @num = []
-        scan.each_char do |s|
-            # p s
-            case s
-            when /[\+\-\*\/()]/ # 符号の場合
-                @scanner << (@num.join).to_i if @num.size >= 1
-                @scanner << @keywords[s]
-                @num.clear if @num.size >= 1
-            when /\d/ # 数字の場合
-                @num << s.to_i # 一度@numに入れる。2桁以上の数値に対応するため
-            end
+    # ソースコードの先頭から、次のtokenを一つ切り出して返す。
+    def get_token()
+        if scan = @scanner.scan(/\A\d+/) then #数値だったら
+            # p "数値：#{scan}"
+            return scan.to_f
+        elsif scan = @scanner.scan(/[\+\-\*\/()]/) then # 予約語だったら
+            # p "予約語：#{@keywords[scan]}"
+            return @keywords[scan]
         end
-        @scanner << (@num.join).to_i if @num.size >= 1 #式の最後に数字があったら
-        # p "#{@scanner}, tokenize"
     end
 
+    # tokenを受け取り、ソースコードの先頭にそれを押し戻す。
+    def unget_token()
+        if !(@scanner.eos?) then
+            @scanner.unscan
+        end
+    end
 
+    # 式
     def expression()
         result = term()
         token = get_token()
         while token == :add or token == :sub
             result = [token, result, term()]
-            p result
             token = get_token()
         end
-        unget_token(token)
-        
+        unget_token()
+        p result
         return result
     end
 
@@ -64,22 +58,25 @@ class Calc
         token = get_token()
         while token == :mul or token == :div
             result = [token, result, factor()]
-            p result
             token = get_token()
         end
-        unget_token(token)
 
+        unget_token()
         return result
     end
 
     def factor()
-        token = get_token
-        if token.is_a?(Numeric) # 数字だったら
+        token = get_token()
+        if token.is_a?(Float) then# 数字だったら
             result = token
-        elsif token == :left_parn #（ だったら
+        elsif token == :get then# 標準入力だったら
+            result = token
+        elsif token.is_a?(String) then# 数字だったら
+            result = [:variable, token]
+        elsif token == :left_parn then#（ だったら
             result = expression()
-            token = get_token # 閉じカッコを取り除く(使用しない)
-            if token != :right_parn # ) なかったら
+            token = get_token() # 閉じカッコを取り除く(使用しない)
+            if token != :right_parn then# ) なかったら
                 raise Exception, "構文エラー"
             else
                 # p token
@@ -93,16 +90,28 @@ class Calc
         if exp.instance_of?(Array)
             case exp[0]
             when :add
-                return eval(exp[1]) + eval(exp[2])
+                begin
+                    return eval(exp[1]) + eval(exp[2])
+                rescue => e
+                    puts "加算でエラー：#{e.message}"
+                end
             when :sub
-                return eval(exp[1]) - eval(exp[2])
+                begin
+                    return eval(exp[1]) - eval(exp[2])
+                rescue => e
+                    puts "減算でエラー：#{e.message}"
+                end
             when :mul
-                return eval(exp[1]) * eval(exp[2])
+                begin
+                    return eval(exp[1]) * eval(exp[2])
+                rescue => e
+                    puts "乗算でエラー：#{e.message}"
+                end
             when :div
                 begin
                     return eval(exp[1]) / eval(exp[2])
                 rescue => e
-                    puts e.message
+                    puts "除算でエラー：#{e.message}"
                 end
             end
         else
@@ -112,23 +121,6 @@ class Calc
 
     def parse()
         expression()
-    end
-
-    # ソースコードの先頭から、次のtokenを一つ切り出して返す。
-    def get_token()
-        if @scanner[0]
-            token = @scanner[0]
-            @scanner = @scanner[1..-1]
-
-            return token
-        end
-    end
-
-    # tokenを受け取り、ソースコードの先頭にそれを押し戻す。
-    def unget_token(token)
-        if token
-            @scanner.unshift(token)
-        end
     end
 end
 
